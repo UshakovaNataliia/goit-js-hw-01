32da961fb650b764a4dae99ee52bde2c0b5bb329
UshakovaNataliia <65922563+UshakovaNataliia@users.noreply.github.com>
2020-07-30 18:00:42 +0300

    Update index.js

--- module-6/index.js
+++ module-6/index.js
@@ -68,7 +68,8 @@
 //   return users.filter(user => user.age>min && user.age<max);
 // };
 
-// console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+// console.log(getUsersWithAge(users, 20, 30)); 
+// [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 
 // console.log(getUsersWithAge(users, 30, 40));
 // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
